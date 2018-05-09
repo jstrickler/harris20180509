9d2e55f0a122e67e1885c3c07f4f9da78843f136
jstrickler <jstrickler@gmail.com>
2018-05-09 12:28:20 -0400

    string examples

--- string_examples.py
+++ string_examples.py
@@ -1,1 +1,25 @@
 #!/usr/bin/env python
+
+s1 = "spam\n"
+s2 = 'spam\n'
+s3 = """spam\n"""
+s4 = '''spam\n'''
+
+print("Python's pretty cool")
+print("It's a good thing")
+print('the "best" thing ever')
+
+print("""No, really, it's the "best" thing ever!""")
+
+print(r"\t\r\b\n\f")
+
+query = """
+select *
+from customers
+where last_name = "Smith"
+"""
+
+
+
+
+
