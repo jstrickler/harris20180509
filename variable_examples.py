c7be2af77d92c0579d8138068a15e617ee199a54
jstrickler <jstrickler@gmail.com>
2018-05-09 12:19:00 -0400

    variables

---
+++ variable_examples.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+x = 5
+y = x
+
+x = 10
+
+y = 20
+
+initial_value = 0.0
+
+# print(z)
+
+thing = None
+
+class_level = 'Freshman'
+
+lunch_break = '11:30'
+
+
+dir = 'east'
+max = 25
+min = 0
+id = 'jstrickler1234'
+
+
+color = 'blue'
+
+value = 25.6
+
+
+# result = color + value
+
+# print(result)
+
+print('-' * 60)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
